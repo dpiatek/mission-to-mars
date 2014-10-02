ec6f3af8dd66bcd7d3226d8a8e128e656f970efd
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-10-02 21:08:06 +0100

    Add exploration area spec

---
+++ tests/exploration_area_spec.js
@@ -0,0 +1,22 @@
+describe("Exploration area", function() {
+  // var ExplorationArea = require("../src/modules/exploration_area");
+  var areaSizeMock = { xAxis: 5, yAxis: 3 };
+  var area;
+
+  beforeEach(function() {
+    area = new ExplorationArea(areaSizeMock);
+  });
+
+  it("creates an area object with an x axis", function() {
+    expect(area.xAxis).toEqual(5);
+  });
+
+  it("reveals out of bounds coordinates", function() {
+    expect(area.isOutOfBound(6, 1)).toBeTruthy();
+  });
+
+  it("reveals fatal coordinates", function() {
+    area.markAsFatal(4, 3);
+    expect(area.isFatal(4, 3)).toBeTruthy();
+  });
+});
