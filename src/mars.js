de46251fac28a7f5da29c045cb0fd9700985116f
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-10-04 19:23:47 +0100

    Add browser interface

--- src/mars.js
+++ src/mars.js
@@ -1,5 +1,23 @@
 var fileInput = require('./modules/file_reader');
+var resultsFormatter = require('./modules/results_formatter');
 
-fileInput(document, 'input', function(instructions) {
-  console.log(instructions);
+var instructionsParser = require('./modules/instructions_parser');
+var ExplorationArea = require('./modules/exploration_area');
+var Robot = require('./modules/robot');
+
+fileInput(document, 'input', function(input) {
+  var instructions = instructionsParser(input);
+
+  var area = new ExplorationArea(instructions.areaSize);
+
+  var results = instructions.robots.map(function(mission) {
+    var robot = new Robot(mission.id);
+    robot.deploy(area, mission.start);
+    robot.explore(mission.instructions);
+    return robot.results();
+  });
+
+  document
+    .getElementById('output')
+    .textContent = resultsFormatter(results);
 });
